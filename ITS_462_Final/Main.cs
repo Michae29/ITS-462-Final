57bcbbc405a8f7d5b71de4f5b05b8dda48e913ed
James Michaels <michae29@purdue.edu>
2020-11-18 15:19:47 -0600

    Modifying Xpath query to pull proper records from website

--- ITS_462_Final/Main.cs
+++ ITS_462_Final/Main.cs
@@ -23,7 +23,11 @@ namespace ITS_462_Final
 
         private void Main_Load(object sender, EventArgs e)
         {
-            
+
+            Item_Deter.SelectedIndex = 0;
+            Price_Deter.SelectedIndex = 2;
+            Scrape_Box.SelectedIndex = 1;
+            Manufacturer_Deter.SelectedIndex = 4;
         }
 
         private void Scrape_Deter_SelectedIndexChanged(object sender, EventArgs e)
@@ -33,13 +37,13 @@ namespace ITS_462_Final
 
         private void Scrape_Button_Click(object sender, EventArgs e)
         {
-             string url = "https://webscraper.io/test-sites/e-commerce/allinone";
+            string url = "https://webscraper.io/test-sites/e-commerce/allinone";
 
-            if (Item_Deter.SelectedItem.ToString()=="Computer")
+            if (Item_Deter.SelectedItem.ToString() == "Computer")
             {
-                  url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops";
+                url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops";
             }
-            else if(Item_Deter.SelectedItem.ToString() == "Tablet")
+            else if (Item_Deter.SelectedItem.ToString() == "Tablet")
             {
                 url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets";
             }
@@ -48,17 +52,26 @@ namespace ITS_462_Final
                 url = "https://webscraper.io/test-sites/e-commerce/allinone/phones/touch";
             }
 
+           
             var web = new HtmlWeb();
             var doc = web.Load(url);
+
+            string itemType = Item_Deter.SelectedItem.ToString();
+            string Manu = Manufacturer_Deter.SelectedItem.ToString();
+            string price = Price_Deter.SelectedItem.ToString();
             string selector = Scrape_Box.SelectedItem.ToString();
 
-            HtmlNode[] nodes = doc.DocumentNode.SelectNodes("//a").Where(x => x.InnerText.Contains("Lenovo")).ToArray();
+            HtmlNode[] nodes = doc.DocumentNode.SelectNodes("//div [@class='caption'] | ./h4 [@class='pull-right price>1000.00']").Where(x => x.InnerText.Contains(Manu)).ToArray();
 
-            foreach(HtmlNode item in nodes)
+            for(int i =0;i<nodes.Length;i++)
             {
-                Console.WriteLine(item.InnerText);
-            }
+                Console.WriteLine("Scrape ran");
 
+               
+
+                Console.WriteLine(nodes[i].InnerHtml);
+            }
+            
 
         }
 
